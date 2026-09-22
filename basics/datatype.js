3f59e7fb5f16814ceec9d9a939f6111d9c0f5e5e
Piyush Pachauri(24BCS11788) <piyushpachauri.18@gmail.com>
2026-09-22 21:03:01 +0000

    commit

---
+++ basics/datatype.js
@@ -0,0 +1,20 @@
+let name ="John Doe"; 
+let age = 30;
+let isloggedIn = true;
+
+//number => 2 to power 53
+
+//bigint
+//string =>""
+//boolean=>true/false
+//null=>standalone value
+//undefined
+//symbol=>unique and immutable
+
+
+//object => key value pair
+
+console.log(typeof name); // string
+console.log(typeof age); // number
+console.log(typeof isloggedIn); // boolean
+console.log(typeof null); // object (this is a known quirk in JavaScript)
